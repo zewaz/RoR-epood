e339dd68d3ae7044ad3cb820cb2a92f4a39abb9c
Rauno Kiss <zewaz@ut.ee>
2012-02-22 13:19:32 +0200

    Initial framework commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoREpood::Application.config.secret_token = 'e2b71c233cff3e9acad814d39d1a74b5bafe4919d4585b0d98646a3bf99fd11e5132ba1fdfa014e55f7826b48dcc83f3eea166502666db0066393601f2d5df83'
