ef48448cec6b634536323b9814929831b7e9a1af
raidoseene <raidoseene@gmail.com>
2012-06-01 13:42:09 +0300

    User models are added, some tests, soon the password thing is coming.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531153326) do
+ActiveRecord::Schema.define(:version => 20120601102920) do
 
   create_table "items", :force => true do |t|
     t.string   "name"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20120531153326) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
